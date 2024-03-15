cf270fa4dbdf4e43f1d3109a0c7b79b2fee712de
subrabala <subrabala.dash@gmail.com>
2024-03-15 22:17:25 +0530

    home-page

--- src/app/success/page.js
+++ src/app/success/page.js
@@ -2,10 +2,11 @@
 import React from "react";
 import { useEffect, useRef } from "react";
 import lottie from "lottie-web";
+import { useRouter } from "next/navigation";
 
 export default function MyComponent() {
   const animationContainer = useRef(null);
-
+  const router = useRouter();
   useEffect(() => {
     lottie.loadAnimation({
       container: animationContainer.current,
@@ -19,6 +20,7 @@ export default function MyComponent() {
   return (
     <div className="h-screen  flex justify-center align-middle">
       <div ref={animationContainer}></div>
+      <p onClick={()=>{router.push('/')}}>Go back to Home Page</p>
     </div>
   );
 }
